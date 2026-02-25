04ed351cf334e31c1f24eb86dde40b22d8c1f2e1
KardeniaPoyu <yirongyiburong@gmail.com>
2026-02-25 22:41:45 +0800

    Update train_sft.py

--- train_sft.py
+++ train_sft.py
@@ -121,10 +121,7 @@ def train_sft(args):
         optim="adamw_torch",
         bf16=True,
         max_grad_norm=1.0,
-        dataloader_num_workers=4,
-        dataloader_pin_memory=True,
-        dataloader_prefetch_factor=2,
-        dataloader_persistent_workers=True,
+        dataloader_num_workers=0,
     )
 
     from trl import SFTTrainer
